eedf7dfe50a4af775f075af93019d8a2ded6feaf
Wasim A Pinjari <wasimapinjari@gmail.com>
2024-06-16 22:22:43 +0530

    fix

--- app/_components/MenuBox.tsx
+++ app/_components/MenuBox.tsx
@@ -23,6 +23,7 @@ export default function MenuBox() {
     // if (state.isSoundOn) music.play();
     // if (!state.isSoundOn) music.paused;
   }
+  console.log(state);
   return (
     <>
       {/* {state.isSoundOn ? (
