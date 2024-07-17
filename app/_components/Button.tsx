a236503bdada47cc7b63ceb63389c27a91d38f5b
Wasim A Pinjari <wasimapinjari@gmail.com>
2024-07-17 21:18:27 +0530

    fix

--- app/_components/Button.tsx
+++ app/_components/Button.tsx
@@ -2,9 +2,9 @@ import useSound from '@/hooks/useSound';
 import { ButtonProps } from '@/types/gameTypes';
 
 export default function Button(props: ButtonProps) {
-  const { handleHoverSound } = useSound();
+  const { hoverSound } = useSound();
   return (
-    <button type='button' onMouseEnter={handleHoverSound} {...props}>
+    <button type='button' onMouseEnter={hoverSound} {...props}>
       {props.children}
     </button>
   );
